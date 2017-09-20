0c04abdc9a6ad632f5ea224d1474b5baa79c9946
Mike Fiedler <miketheman@gmail.com>
2017-09-20 07:14:48 -0400

    Refactor site selection logic to use importlib
    Instead of trying to maintain a list of known sites, importing them and
    then using an if statement to determine which one to use, leverage some
    patterns and match against module that has to be named after the site.
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- statusbot/status_check.py
+++ statusbot/status_check.py
@@ -1,33 +1,26 @@
+import importlib
+
 from tldextract import TLDExtract
-
-from .sites import github_site
-from .sites import twitter_site
-
-KNOWN_SITES = [
-    'github',
-    'twitter',
-]
 
 
 def check_site(site):
     site_name = normalize_site_name(site)
 
-    if site_name not in KNOWN_SITES:
+    try:
+        module_name = 'statusbot.sites.' + site_name + '_site'
+        imported_module = importlib.import_module(module_name)
+    except ImportError:
         raise NotImplementedError
 
     print("Checking site: {}".format(site))
-    # TODO: There must be a better way to call the site checks without a large if/elif
-    if site_name == 'github':
-        status = github_site.status()
-    elif site_name == 'twitter':
-        status = twitter_site.status()
+    status = imported_module.status()
 
     return site_name, status
 
 
 def normalize_site_name(site_name):
     """Receive a string and compare against known sites and convert to internal name
-    Return site domain wihtout any other elements.
+    Return site domain without any other elements.
     """
     # Prevent the default behavior of TLDExtract making an outbound request to cache TLDs
     tldextractor = TLDExtract(suffix_list_urls=False)
